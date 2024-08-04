c4ec0bc834d729501efd7438f30bda8d06772f96
ayxn07 <162783391+ayxn07@users.noreply.github.com>
2024-08-04 13:51:50 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function handleLogin(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === 'Ayxn07' && password === 'Amazon@786') {
+        window.location.href = 'https://www.youtube.com';
+    } else {
+        alert('Invalid username or password');
+    }
+}
