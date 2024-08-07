3278392d7676dff5ab1a18a88a0000a3e4cdfdc2
ayxn07 <162783391+ayxn07@users.noreply.github.com>
2024-08-07 12:03:13 +0400

    Update indexScript.js

--- indexScript.js
+++ indexScript.js
@@ -3,7 +3,7 @@ function handleLogin(event) {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    if (username === 'Ayxn07' && password === '12345') {
+    if (username === 'Ayxn07' && password === 'Mommy') {
         window.location.href = 'menu.html';
     } else {
         alert('Invalid username or password');
